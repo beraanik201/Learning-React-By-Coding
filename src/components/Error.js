10c82510fcb2f4b82b758f8c7b13a0f04dc7952c
beraanik201 <beraanik201@gmail.comgit config --global user.email beraanik201@gmail.com>
2025-10-17 09:10:40 +0530

    FEATURE: React Router Basics

---
+++ src/components/Error.js
@@ -0,0 +1,15 @@
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+  const err = useRouteError();
+  return (
+    <div>
+      <h1>
+        {err.status}: {err.statusText}
+      </h1>
+      <h2>This page is not found</h2>
+    </div>
+  );
+};
+
+export default Error;
