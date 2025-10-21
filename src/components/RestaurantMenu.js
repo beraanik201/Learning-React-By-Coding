4bab205ba1bd82fbd8d0b66eb9ee027aa60e186d
beraanik201 <beraanik201@gmail.comgit config --global user.email beraanik201@gmail.com>
2025-10-21 18:02:29 +0530

    FEATURE: Implementing Custom Hooks Functionality

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,73 +1,15 @@
-import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
-// import { RESTAURANT_MENU_IMAGE_URL } from "../utils/constants";
 import ItemCategory from "./ItemCategory";
 import NestedItemCategory from "./NestedItemCategory";
 import { useParams } from "react-router-dom";
-import { RESTAURANT_MENU } from "../utils/constants";
+import useRestaurantInfo from "../utils/useRestaurantInfo";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
-  const [resMenu, setResMenu] = useState([]);
-
   const { resId } = useParams();
 
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-
-  const fetchMenu = async () => {
-    try {
-      const data = await fetch(RESTAURANT_MENU + resId);
-      const json = await data.json();
-
-      const menuData = json?.data?.cards
-        ?.find((obj) => obj?.groupedCard)
-        ?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
-          (obj) =>
-            obj?.card?.card["@type"]?.includes("ItemCategory") ||
-            obj?.card?.card["@type"]?.includes("NestedItemCategory")
-        );
-
-      const organisedMenuData = menuData?.map((item) => {
-        const type = item?.card?.card["@type"];
-        const title = item?.card?.card?.title;
-        const itemCards = item?.card?.card?.itemCards || [];
-        const categories = item?.card?.card?.categories || [];
-
-        if (type?.includes("NestedItemCategory")) {
-          return {
-            title,
-            type: "nested",
-            categories: categories.map((subCategories) => {
-              return {
-                title: subCategories?.title,
-                itemCards: subCategories?.itemCards,
-              };
-            }),
-          };
-        } else {
-          return {
-            title,
-            type: "item",
-            itemCards,
-          };
-        }
-      });
-
-      const restaurentMenu = json?.data?.cards?.find((item) =>
-        item?.card?.card["@type"]?.includes("food.v2.Restaurant")
-      );
-
-      const restaurantInfo = restaurentMenu?.card?.card?.info;
-
-      setResInfo(restaurantInfo);
-
-      setResMenu(organisedMenuData);
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
+  const resInfo = useRestaurantInfo(resId);
+  const resMenu = useRestaurantMenu(resId);
 
   if (resInfo === null) return <Shimmer />;
 
@@ -76,11 +18,11 @@ const RestaurantMenu = () => {
 
   return (
     <div className="menu">
-      <h2>{name}</h2>
+      <h2>{name ?? ""}</h2>
       <p>{cuisines.join(", ")}</p>
       <p>{costForTwoMessage}</p>
-      <p>{avgRating} ⭐</p>
-      <p>Delivery Time: {deliveryTime} mins</p>
+      <p>{avgRating ?? 0} ⭐</p>
+      <p>Delivery Time: {deliveryTime ?? 0} mins</p>
 
       <h1>Menu</h1>
 
