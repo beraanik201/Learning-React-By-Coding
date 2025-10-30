d057e6c56e1f722fc6cf56a6755b6aabcfb0e4fe
beraanik201 <beraanik201@gmail.comgit config --global user.email beraanik201@gmail.com>
2025-10-30 22:27:50 +0530

    FEATURE: Enhancing The Project4

--- src/components/Body.js
+++ src/components/Body.js
@@ -18,7 +18,9 @@ const Body = () => {
     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING";
 
   const fetchData = async () => {
-    const data = await fetch(`https://proxy.corsfix.com/?${swiggyApiUrl}`);
+    const data = await fetch(
+      `/api/swiggy?url=${encodeURIComponent(swiggyApiUrl)}`
+    );
 
     const json = await data.json();
 
